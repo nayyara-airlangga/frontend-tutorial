946c6a2b10e869235c9849304905f1d2d0ec40c5
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2024-01-31 23:53:58 +0700

    add error commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
 // Import and init Sentry SDK
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://e349b25f82abe9753d85f7139f41c112@o4506666514972672.ingest.sentry.io/4506666574675968",
+  integrations: [new Sentry.BrowserTracing(), Sentry.replayIntegration()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 const container = document.getElementById("root");
 const root = createRoot(container);
